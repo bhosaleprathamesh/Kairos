ca65bb380a8c573ecd05d18592e414acc1c6db54
PrathameshBhosale <bprathamesh108@gmail.com>
2025-01-17 20:33:38 +0530

    nvm credits

--- kairos.py
+++ kairos.py
@@ -306,7 +306,7 @@ else:
 st.markdown(
     """
     <footer style="padding: 10px; margin-top: 50px;">
-        <a href="https://www.weatherapi.com/" title="Free Weather API"><img src='//cdn.weatherapi.com/v4/images/weatherapi_logo.png' alt="Weather data by WeatherAPI.com" border="0"></a>
+        <p>Powered by <a href="https://www.weatherapi.com/" title="Free Weather API">WeatherAPI.com</a></p>
     </footer>
     """,
     unsafe_allow_html=True
